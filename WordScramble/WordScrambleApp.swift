994b982f1c0b5860c8041e2ad242b3dd929be6fa
Meruem Moniker <jamesconnorhill@gmail.com>
2024-02-09 08:04:43 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Connor Hill on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
